1393f71d10a657ca25d0af19cc040b8d5632dcf7
okumu87 <okumu793@gmail.com>
2025-07-11 12:29:07 +0200

    calculator

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-
-const inputValue = document.getElementById('userinput');
+const inputValue = document.getElementById('userInput');
 
 const calculate = document.querySelectorAll('.operations').forEach(function(item){
-    item.addEventListener('click', function(e){
+    console.log(item);
+
+    item.addEventListener('click', function(e) {
         let lastValue = inputValue.innerText.substring(inputValue.innerText.length, inputValue.innerText.length - 1);
-        if(!isNaN (lastValue && e.target.innerText === '=')){
-            inputValue.innerText = eval(inputValue.innerText)
-        }else if (e.target.innerText === 'AC'){
-            inputValue.innerText= '0';
-        }else if (e.target.innerText === 'DEL'){
+        if (!isNaN(lastValue) && e.target.innerText === '='){
+            inputValue.innerText  = eval(inputValue.innerText);
+        }else if(e.target.innerText === 'AC'){
+                inputValue.innerText = '0';
+        } else if(e.target.innerText === 'DEL'){
             inputValue.innerText = inputValue.innerText.substring(0, inputValue.innerText.length - 1);
         }
-        
-        if (inputValue.innerText.length === 0 ){
+        if(inputValue.innerText.length == 0){
             inputValue.innerText = '0';
-        }
-        else{
-
+        } else{
             if(!isNaN(lastValue)){
-                inputValue.innerText += e.target.innerText;
+            inputValue.innerText += e.target.innerText;
             }
-           
-        }
-    });
+         
+        }      
+})
 });
 
+
 const number = document.querySelectorAll('.number').forEach(function(item){
-    item.addEventListener('click', function(e){
-        if(inputValue.innerText === '0'){
-            inputValue.innerText = '';
-        }
-        inputValue.innerText += e.target.innerText.trim();
-    })
+   item.addEventListener('click', function(e){
+    if(inputValue.innerText === '0'){
+        inputValue.innerText = '';
+    }
+inputValue.innerText += e.target.innerText.trim();
+   })
 });
